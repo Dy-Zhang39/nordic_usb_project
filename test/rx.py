55fc0b2706e271cd7185b14464a9ef94a892b419
Dy-Zhang39 <zhangshaoyang39@gmail.com>
2023-07-27 10:23:16 -0400

    added data flow control to the tester

--- test/rx.py
+++ test/rx.py
@@ -1,41 +1,51 @@
 import serial
 import time
 
+# test parameters:
+pkt_sz = 128 # packet size, need to match the sender packet size
+test_data_sz = 100000 # total data to be sent
+
 # signal
-done = "1"
-done = done.encode('utf-8')
+ack = b'ACK' # ack signal
+
+# done signal
+done = pkt_sz * b'D'
+
+
+total_data_received = 0
 
 # open receiver port
 receiver = serial.Serial('/dev/tty.usbmodem211203', 115200)
 print("receiver port: " + receiver.name) # check which port was really used
-receiver.write(done) # let sender know that receiver is connected
+receiver.write(ack) # let sender know that receiver is connected
 print("receiver connected")
 
-total_data_received = 0
 # receive data
 print("receiving data...")
 
 start = time.time()
 while True:
-    data = receiver.read(1)
-    # print("data received: " + str(total_data_received))
-    total_data_received += 1
-    receiver.write(done) # write a string with newline
+    data = receiver.read(pkt_sz)
     if data == done:
+        receiver.write(done)
         break
+    else:
+        # print("data received: " + str(total_data_received))
+        # print progress bar
+        print("#", end="", flush=True)
+        total_data_received += len(data)
+        receiver.write(ack) # write a string with newline
 
 end = time.time()
 delta_time = end - start
-print("time taken: " + str(delta_time))
+print("\ntime taken: " + str(delta_time))
 
 
-# send done signal to sender
-receiver.write(done)
 
 # receive the last 4 bytes containing the total data sent
 total_data_sent = receiver.read(4)
 print("total data sent: " + str(int.from_bytes(total_data_sent, byteorder='big')))
-print("received data: " + str(total_data_received - 1)) # minus 1 because of the last done signal
+print("received data: " + str(total_data_received - 1)) # minus 1 because of the last ack signal
 throughput = (total_data_received - 1) * 8/ delta_time
 print("throughput: " + str(throughput) + " bits/second")
 # close port
--- test/tx.py
+++ test/tx.py
@@ -1,36 +1,60 @@
 import serial
 
+# test procedure:
+# 1. start sender, sender will wait for receiver to send ack signal
+# 2. start receiver, receiver will connect to sender and send ack signal
+# 3. after sender receives ack signal, sender will start sending data, and wait for reciever to send ack signal
+# 4. after receiver receives data, receiver will send ack signal to sender
+# 5. repeat step 3 and 4 until total data sent is 1000 bytes
+# 6. after total data sent is 1000 bytes, sender will send done signal to receiver
+# 7. after receiver receives done signal, receiver will send done signal to sender
+# 8. after sender receives done signal, sender will send the total data sent to the receiver as 4 bytes
+# 9. after receiver receives the total data sent, receiver will print out the throughput
+# 10. close the port
 
-done = "1"
-done = done.encode('utf-8')
+# test parameters:
+pkt_sz = 128           # packet size
+test_data_sz = 100000 # total data to be sent
+
+# signal
+ack = b'ACK'            # ack signal
+done = pkt_sz * b'D'
 wait = True
+total_data_sent = 0
 
+#create a data of pkt_sz bytes
+data = pkt_sz * b'a'
 
 # open sender port
 sender = serial.Serial('/dev/tty.usbmodem211201', 115200)
 print("sender port: " + sender.name) # check which port was really used
 
-total_data_sent = 0
+# wait for receiver to connect
 print("waiting for receiver to connect...")
-if sender.read(1) == done:
+if sender.read(len(ack)) == ack:
     wait = False
     print("receiver connected")
 
-while total_data_sent < 100000:
+# send data
+while total_data_sent < test_data_sz:
     if wait == False:
-        sender.write(b'2') # write a string with newline
-        total_data_sent += 1
+        sender.write(data) # write a string with newline
+        total_data_sent += len(data)
+        print("#", end="", flush=True)
         # print("data sent: " + str(total_data_sent))
         wait = True
-    if sender.read(1) == done:
+    if sender.read(len(ack)) == ack:
         wait = False
 
 
-sender.write(done) # write a string with newline
-
-# send the total data sent to the receiver as 4 bytes
-sender.write(total_data_sent.to_bytes(4, byteorder='big'))
-print("sent data: " + str(total_data_sent)) 
+# send done signal to receiver
+sender.write(done)
+print("\n")
+if sender.read(pkt_sz) == done:
+    print("receiver received done signal")
+    # send the total data sent to the receiver as 4 bytes
+    sender.write(total_data_sent.to_bytes(4, byteorder='big'))
+    print("sent data: " + str(total_data_sent)) 
 
 # close port
 sender.close()
